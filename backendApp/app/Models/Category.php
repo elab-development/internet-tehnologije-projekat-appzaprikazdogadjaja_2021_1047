b66f19aa72d4329bc6a9a70066a8dfe17f8600de
Damjan Radojevic <dakyrad@gmail.com>
2024-08-18 15:25:16 +0200

    Edited models

--- backendApp/app/Models/Category.php
+++ backendApp/app/Models/Category.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title', 'description', 'start_date', 'end_date', 'venue_id', 'category_id', 'source_id', 'url', 'image'
+    ];
 }
