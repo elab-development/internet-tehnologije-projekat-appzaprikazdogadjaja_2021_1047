98d393026ffa07c9f1373d80346a9e7ee730d276
Damjan Radojevic <dakyrad@gmail.com>
2024-09-07 23:03:59 +0200

    Added routes

--- frontendapp/src/App.js
+++ frontendapp/src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 import HomePage from './HomePage/HomePage';
 import Login from './Authorization/Login';
 import Register from './Authorization/Register';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Register></Register>
-      <Login />
-      <HomePage></HomePage>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
